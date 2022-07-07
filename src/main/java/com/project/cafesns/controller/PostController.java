8eeb7794ad6a497fb1faa43269cb0ba5ec647317
seungchanKKK <eccsck@gmail.com>
2022-07-07 21:32:04 +0900

    jwt 토큰 id 얻는 로직적용

--- src/main/java/com/project/cafesns/controller/PostController.java
+++ src/main/java/com/project/cafesns/controller/PostController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.NoSuchAlgorithmException;
 
 @RequiredArgsConstructor
@@ -21,22 +22,22 @@ public class PostController {
     @PostMapping("/{cafeId}/posts")
     public ResponseEntity<?> addPost(@PathVariable Long cafeId,
                         @RequestBody PostRequestDto postRequestDto,
-                        Long httpRequest) throws NoSuchAlgorithmException {
-        Long userId =httpRequest;
+                        HttpServletRequest httpRequest) throws NoSuchAlgorithmException {
+        Long userId = userInfoInJwt.getUserId_InJWT(httpRequest.getHeaders("Authorization"));
         postService.addPost(cafeId,postRequestDto,userId);
         return ResponseEntity.ok().body(ResponseDto.builder().result(true).message("게시글이 작성되었습니다.").build());
     }
 
     @PatchMapping("/posts/{postId}")
-    public ResponseEntity<?> updatePost(@PathVariable Long postId, @RequestBody PostPatchDto postPatchDto, Long httpRequest){
-        Long userId =httpRequest;
+    public ResponseEntity<?> updatePost(@PathVariable Long postId, @RequestBody PostPatchDto postPatchDto, HttpServletRequest httpRequest){
+        Long userId = userInfoInJwt.getUserId_InJWT(httpRequest.getHeaders("Authorization"));
         postService.updatePost(postId,postPatchDto,userId);
         return ResponseEntity.ok().body(ResponseDto.builder().result(true).message("게시글이 수정에 성공했습니다.").build());
     }
 
     @DeleteMapping("/posts/{postId}")
-    public ResponseEntity<?>  deletePost(@PathVariable Long postId,Long httpRequest){
-        Long userId =httpRequest;
+    public ResponseEntity<?>  deletePost(@PathVariable Long postId,HttpServletRequest httpRequest){
+        Long userId = userInfoInJwt.getUserId_InJWT(httpRequest.getHeaders("Authorization"));
         postService.deletePost(postId,userId);
         return ResponseEntity.ok().body(ResponseDto.builder().result(true).message("게시글이 삭제되었습니다.").build());
     }
