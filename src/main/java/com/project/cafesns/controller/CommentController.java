3fc8ca1891d00a69479365b5a0913f4eb79f96d6
seungchanKKK <eccsck@gmail.com>
2022-07-07 21:54:38 +0900

    토큰 userid 얻는로직적용

--- src/main/java/com/project/cafesns/controller/CommentController.java
+++ src/main/java/com/project/cafesns/controller/CommentController.java
@@ -9,28 +9,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
     @PostMapping("/posts/{postId}/comments")
-    public ResponseEntity<?> addComment(@PathVariable Long postId, @RequestBody CommentRequestDto commentRequestDto, Long httpRequest){
-        Long userId = httpRequest;
+    public ResponseEntity<?> addComment(@PathVariable Long postId, @RequestBody CommentRequestDto commentRequestDto, HttpServletRequest httpRequest){
+        Long userId = userInfoJwt.getUserId_InJWT(httpRequest.getHeaders("Authorization"));
         commentService.addComment(postId,commentRequestDto,userId);
         return ResponseEntity.ok().body(ResponseDto.builder().result(true).message("댓글이 작성되었습니다!").build());
     }
 
     @PatchMapping("/comments/{commentId}")
-    public ResponseEntity<?> updateComment(@PathVariable Long commentId, @RequestBody CommentRequestDto commentRequestDto, Long httpRequest){
-        Long userId =httpRequest;
+    public ResponseEntity<?> updateComment(@PathVariable Long commentId, @RequestBody CommentRequestDto commentRequestDto, HttpServletRequest httpRequest){
+        Long userId = userInfoJwt.getUserId_InJWT(httpRequest.getHeaders("Authorization"));
         commentService.updateComment(commentId,commentRequestDto,userId);
         return ResponseEntity.ok().body(ResponseDto.builder().result(true).message("댓글이 수정되었습니다!").build());
     }
 
     @DeleteMapping("/comments/{commentId}")
-    public ResponseEntity<?>  deleteComment(@PathVariable Long commentId,Long httpRequest){
-        Long userId =httpRequest;
+    public ResponseEntity<?>  deleteComment(@PathVariable Long commentId,HttpServletRequest httpRequest){
+        Long userId = userInfoJwt.getUserId_InJWT(httpRequest.getHeaders("Authorization"));
         commentService.deleteComment(commentId,userId);
         return ResponseEntity.ok().body(ResponseDto.builder().result(true).message("댓글이 삭제되었습니다!").build());
     }
