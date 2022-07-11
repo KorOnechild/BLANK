57dcc3ab8d2a88f4b38619489739772742178af5
KorOnechild <kyuhong99@gmail.com>
2022-07-11 12:52:25 +0900

    webconfig 설정

--- src/main/java/com/project/cafesns/webconfig/WebConfig.java
+++ src/main/java/com/project/cafesns/webconfig/WebConfig.java
@@ -1,5 +1,6 @@
 package com.project.cafesns.webconfig;
 
+import com.project.cafesns.jwt.JwtTokenInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
@@ -9,10 +10,11 @@ import org.springframework.web.servlet.config.annotation.*;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    private final JwtTokenInterceptor jwtTokenInterceptor;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("localhost:8080")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
@@ -23,5 +25,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**")
                 .addResourceLocations("file:///home/ubuntu/upload/");
+    }
+
+    //토큰을 받아야 하는 서비스 설정
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry
+                .addInterceptor(jwtTokenInterceptor) //로그인이 필요한 서비스 요청시 Interceptor가 그 요청을 가로챔
+                .addPathPatterns("/api/user/signout")
+                .addPathPatterns("/api/auth/refresh")
+                .addPathPatterns("/api/upload")
+                .addPathPatterns("/admin/**");
     }
 }
