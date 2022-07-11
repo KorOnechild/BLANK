0a76cae78fad0f80d9282e1ba12eac3d6955903e
KorOnechild <kyuhong99@gmail.com>
2022-07-11 14:46:09 +0900

    마이페이지 게시글 목록 조회 비즈니스 로직/ 필요 함수 구성

--- src/main/java/com/project/cafesns/controller/PostListController.java
+++ src/main/java/com/project/cafesns/controller/PostListController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class PostListController {
 
@@ -15,4 +17,12 @@ public class PostListController {
     public ResponseEntity<?> getPostListOrderByDesc(@PathVariable String region){
         return postListService.getPostListOrderByDesc(region);
     }
+
+    @GetMapping("/api/user/posts")
+    public ResponseEntity<?> getUserPostList(HttpServletRequest request){
+
+        return postListService.getUserPostList(request);
+    }
+
+
 }
--- src/main/java/com/project/cafesns/service/PostListService.java
+++ src/main/java/com/project/cafesns/service/PostListService.java
@@ -1,15 +1,14 @@
 package com.project.cafesns.service;
 
 import com.project.cafesns.model.dto.ResponseDto;
-import com.project.cafesns.model.dto.post.PostListDto;
-import com.project.cafesns.model.entitiy.Cafe;
-import com.project.cafesns.model.entitiy.Post;
-import com.project.cafesns.repository.CafeRepository;
-import com.project.cafesns.repository.PostListRepository;
+import com.project.cafesns.model.dto.postlist.*;
+import com.project.cafesns.model.entitiy.*;
+import com.project.cafesns.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,24 +16,108 @@ import java.util.List;
 @Service
 public class PostListService {
 
+    private final UserInfoInJwt userInfoInJwt;
+
+    private final CommentRepository commentRepository;
+    private final LikeRepository likeRepository;
     private final PostListRepository postListRepository;
     private final CafeRepository cafeRepository;
+    private final ImageRepository imageRepository;
+
+    private final HashtagRepository hashtagRepository;
+
+    //메인페이지 게시글 목록 최신순 조회
     public ResponseEntity<?> getPostListOrderByDesc(String region) {
         List<Cafe> cafeList = cafeRepository.findAllByAddressContaining(region);
-        List<PostListDto> postListDtos = new ArrayList<>();
+        List<MainPostListDto> postListDtos = new ArrayList<>();
 
 
         for(Cafe cafe : cafeList){
             List<Post> postList = postListRepository.findAllByCafeOrderByModifiedAtDesc(cafe);
-            for(Post post : postList){PostListDto postListDto = new PostListDto();
+            for(Post post : postList){
+                MainPostListDto postListDto = new MainPostListDto();
                 postListDto.setCafeid(cafe.getId());
                 postListDto.setCafename(cafe.getCafename());
-                postListDto.setImg(post.getImgList().get(0));
+                postListDto.setImg(getImageDtoList(post).get(0).getImg());
                 postListDto.setPostid(post.getId());
                 postListDtos.add(postListDto);
             }
         }
 
-        return ResponseEntity.ok().body(ResponseDto.builder().result(true).message("지역목록 조회에 성공했습니다."))
+        return ResponseEntity.ok().body(ResponseDto.builder().result(true).message("지역목록 조회에 성공했습니다.").data(postListDtos).build());
+    }
+
+    //마이페이지 게시글 목록 조회
+    public ResponseEntity<?> getUserPostList(HttpServletRequest request) {
+        userInfoInJwt.getUserInfo_InJwt(httpServletRequest.getHeader("Authorization"));
+
+        User user = userRepository.findById(userInfoInJwt.getUserid()).orElseThrow(
+                ()-> new NullPointerException("사용자 정보가 없습니다.")
+        );
+
+        List<Post> postList = postListRepository.findAllByUserOrderByModifiedAtDesc(user);
+        List<UserPostListDto> userPostListDtos = new ArrayList<>();
+
+        for(Post post : postList){
+            userPostListDtos.add(
+                    UserPostListDto.builder().postid(post.getId())
+                    .nickname(user.getNickname())
+                    .image(getImageDtoList(post))
+                    .hashtagList(getHashtagDtoList(post))
+                    .modifiedAt(post.getModifiedAt())
+                    .star(post.getStar())
+                    .likecnt(getLikeCnt(post))
+                    .commentCnt(getCommentCnt(post))
+                    .commentList(getCommentDtoList(post)));
+        }
+        return ResponseEntity.ok().body(ResponseDto.builder().result(true).message("내가 쓴 리뷰 목록을 조회했습니다.").data(userPostListDtos).build());
+
+    }
+
+    //게시글 좋아요 개수 얻기 로직
+    public int getLikeCnt(Post post){
+        return likeRepository.findAllByPost(post).size();
+    }
+
+    //댓글 개수 얻기 로직
+    public int getCommentCnt(Post post){
+        return commentRepository.findAllByPost(post).size();
+    }
+
+    //imageDto 리스트 만들기 로직
+    public List<ImageDto> getImageDtoList(Post post){
+        List<Image> imageList = imageRepository.findAllByPost(post);
+        List<ImageDto> imageDtos = new ArrayList<>();
+        for(Image image : imageList){
+            imageDtos.add(ImageDto.builder().img(image.getImg()).build());
+        }
+        return imageDtos;
+    }
+
+
+    //hashtagDto 리스트 만들기 로직
+    public List<HashtagDto> getHashtagDtoList(Post post){
+        List<Hashtag> hashtagList = hashtagRepository.findAllByPost(post);
+        List<HashtagDto> hashtagDtos = new ArrayList<>();
+
+        for(Hashtag hashtag : hashtagList){
+            hashtagDtos.add(HashtagDto.builder().hashtag(hashtag.getHashtag()).build());
+        }
+        return hashtagDtos;
+    }
+
+    //commentDto 리스트 만들기 로직
+    public List<CommentDto> getCommentDtoList(Post post){
+        List<Comment> commentList = commentRepository.findAllByPostOrderByDesc(post);
+        List<CommentDto> commentDtos = new ArrayList<>();
+        for(Comment comment : commentList){
+            commentDtos.add(
+                    CommentDto.builder()
+                    .profileimg(comment.getUser().getProfileimg())
+                    .nickname(comment.getUser().getNickname())
+                    .contents(comment.getContents())
+                    .build());
+        }
+        return commentDtos;
     }
 }
