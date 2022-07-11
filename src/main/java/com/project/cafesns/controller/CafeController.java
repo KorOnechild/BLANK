8847da6214fc45290f709f1e992fa70828cd3b31
waryongc <jip95@naver.com>
2022-07-11 11:56:50 +0900

    카페 상세 페어지 // 카페 배너 조회

--- src/main/java/com/project/cafesns/controller/CafeController.java
+++ src/main/java/com/project/cafesns/controller/CafeController.java
@@ -1,12 +1,23 @@
 package com.project.cafesns.controller;
 
 
+import com.project.cafesns.service.CafeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class CafeController {
+
+    private final CafeService cafeService;
+
+    // 카페 상세 페이지 배너 조회
+    @GetMapping("/api/cafes/{cafeId}")
+    public ResponseEntity<?> getBanner (Long cafeId){
+        return cafeService.getBanner(cafeId);
+    }
 
 }
 
--- src/main/java/com/project/cafesns/service/CafeService.java
+++ src/main/java/com/project/cafesns/service/CafeService.java
@@ -1,9 +1,61 @@
 package com.project.cafesns.service;
 
+import com.project.cafesns.model.dto.ResponseDto;
+import com.project.cafesns.model.entitiy.Image;
+import com.project.cafesns.model.entitiy.Post;
+import com.project.cafesns.repository.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CafeService {
+
+    private final PostRepository postRepository;
+    private final ImageRepository imageRepository;
+    private final CafeRepository cafeRepository;
+
+    private final MenuRepository menuRepository;
+
+    private final UserRepository userRepository;
+
+    // 카페 상세 페이지 배너조회
+    public ResponseEntity<?> getBanner(Long cafeId) {
+        List<Post> postList = postRepository.findAllByCafeId(cafeId);
+//       List<Image> imageList = postRepository.findByCafeId(cafeId).get();
+        List<Image> imageList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            imageList.add(imageRepository.findOneById(postList.get(i).getId()));
+        }
+        String logoimg = cafeRepository.findByCafeId(cafeId).getUser().getLogoimg();
+        String cafename = cafeRepository.findByCafeId(cafeId).getCafename();
+//       int avgstar = 0;
+        int allstar = 0;
+        for (int i = 0; i < postList.size(); i++) {
+            allstar += postList.get(i).getStar();
+        }
+        int avgstar = allstar / postList.size();
+        Long postCnt = Long.valueOf(postRepository.findByCafeId(cafeId).size());
+        String opentime = cafeRepository.findByCafeId(cafeId).getOpentime();
+        String closetime = cafeRepository.findByCafeId(cafeId).getClosetime();
+
+        return ResponseEntity.ok().body(ResponseDto.builder()
+                .result(true)
+                .message("배너 조회에 성공했습니다.")
+                .data(CafeBannerDto.builder()
+                        .imageList(imageList)
+                        .logoimg(logoimg)
+                        .cafename(cafename)
+                        .avgstar(avgstar)
+                        .postCnt(postCnt)
+                        .opentime(opentime)
+                        .closetime(closetime)
+                        .build())
+                .build());
+
+    }
 }
