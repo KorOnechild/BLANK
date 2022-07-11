e6e40558273946ffca7074d7e2a0634a9aea14ea
waryongc <jip95@naver.com>
2022-07-11 15:02:58 +0900

    카페 신청 (일반 유저) 컨트롤러 / 서비스

--- src/main/java/com/project/cafesns/controller/RegisterController.java
+++ src/main/java/com/project/cafesns/controller/RegisterController.java
@@ -1,12 +1,33 @@
 package com.project.cafesns.controller;
 
 
+import com.project.cafesns.error.ErrorCode;
+import com.project.cafesns.model.dto.register.RegisterRequestDto;
+import com.project.cafesns.model.entitiy.Register;
+import com.project.cafesns.model.entitiy.User;
+import com.project.cafesns.repository.UserRepository;
+import com.project.cafesns.service.RegisterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class RegisterController {
 
-}
+    private final UserRepository userRepository;
+    private final RegisterService registerService;
 
+    // 카페 신청 (일반유저)
+    @PostMapping("/api/user/regist-cafe")
+    public ResponseEntity<?> applyCafe(HttpServletRequest httpServletRequest,@RequestBody RegisterRequestDto registerRequestDto) {
+        return registerService.applyCafe(httpServletRequest,registerRequestDto);
+    }
+
+}
--- src/main/java/com/project/cafesns/service/RegisterService.java
+++ src/main/java/com/project/cafesns/service/RegisterService.java
@@ -1,9 +1,49 @@
 package com.project.cafesns.service;
 
+import com.project.cafesns.error.ErrorCode;
+import com.project.cafesns.error.exceptions.AlreadyExistCafeOrAddressException;
+import com.project.cafesns.error.exceptions.NoExistUserException;
+import com.project.cafesns.model.dto.ResponseDto;
+import com.project.cafesns.model.dto.register.ApplyListResponseDto;
+import com.project.cafesns.model.dto.register.RegisterRequestDto;
+import com.project.cafesns.model.entitiy.Cafe;
+import com.project.cafesns.model.entitiy.Register;
+import com.project.cafesns.model.entitiy.User;
+import com.project.cafesns.repository.CafeRepository;
+import com.project.cafesns.repository.RegisterRepository;
+import com.project.cafesns.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RegisterService {
+
+    private final UserRepository userRepository;
+    private final RegisterRepository registerRepository;
+    private final CafeRepository cafeRepository;
+
+    // 유저일 때 신청
+    public ResponseEntity<?> applyCafe(HttpServletRequest httpServletRequest, RegisterRequestDto registerRequestDto) {
+
+        Long userId = userInfoInJwt.getUserId_InJWT(httpServletRequest.getHeader("Authorization"));
+        User user = userRepository.findByUserId(userId).orElseThrow(
+                () -> new NoExistUserException(ErrorCode.NO_EXIST_USER_EXCEPTION)
+        );
+        Register register = Register.builder().registerRequestDto(registerRequestDto).user(user).build();
+
+        if(cafeRepository.existsByCafeByAddressAndCafename(registerRequestDto.getAddress(), registerRequestDto.getCafename())){
+            throw new AlreadyExistCafenameAndAddressException(ErrorCode.ALREADY_EXIST_CAFE_EXCEPTION);
+        }else{
+            registerRepository.save(register); //register Entity에 userId 넣는가! 아니면 저장을 어떻게 같이 하는지
+            return ResponseEntity.ok().body(ResponseDto.builder()
+                    .result(true)
+                    .message("카페 등록 신청이 완료되었습니다.")
+                    .build());
+        }
+    }
 }
