8d672e0b4d667e955e58d93b2b3adbbdff9cfd66
KorOnechild <kyuhong99@gmail.com>
2022-07-12 12:02:20 +0900

    cors 설정

--- src/main/java/com/project/cafesns/webconfig/WebConfig.java
+++ src/main/java/com/project/cafesns/webconfig/WebConfig.java
@@ -14,19 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("localhost:8080", "localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
     }
-
-    //이미지 관련
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/**")
-                .addResourceLocations("file:///home/ubuntu/upload/");
-    }
-
+    
     //토큰을 받아야 하는 서비스 설정
     public void addInterceptors(InterceptorRegistry registry) {
         registry
