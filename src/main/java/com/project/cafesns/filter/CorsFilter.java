b60f8464c7528ba5d6a600d9a5f8a2716fc60050
KorOnechild <kyuhong99@gmail.com>
2022-07-18 13:00:41 +0900

    Cors :  Access-Control-Allow-Headers 추가 설정

--- src/main/java/com/project/cafesns/filter/CorsFilter.java
+++ src/main/java/com/project/cafesns/filter/CorsFilter.java
@@ -15,7 +15,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*"); //허용대상 도메인
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, PATCH, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept, authorization");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Origin, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
         chain.doFilter(req, res);
     }
     public void init(FilterConfig filterConfig) {}
