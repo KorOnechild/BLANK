179de8a45dd60ae64a9bb565c61c5ed4bec1b31b
KorOnechild <kyuhong99@gmail.com>
2022-07-21 00:23:54 +0900

    JVM 시간설정

--- src/main/java/com/project/cafesns/CafeSnsApplication.java
+++ src/main/java/com/project/cafesns/CafeSnsApplication.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class CafeSnsApplication {
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 
 	public static void main(String[] args) {
-		System.setProperty("user.timezone", "Asia/Seoul");
 		SpringApplication.run(CafeSnsApplication.class, args);
 	}
 
