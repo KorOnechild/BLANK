e146a8df50129a4a6e831d6bccf5b0d661a312f3
KorOnechild <kyuhong99@gmail.com>
2022-07-21 16:37:18 +0900

    [chore] PostListRepository와 PostRepository를 병합
    같은 Entity에 대한 query문을 다루고 있어서 두개의 Repository를 한개로 합침 이름은 PostRepository로 결정

--- src/main/java/com/project/cafesns/repository/PostRepository.java
+++ src/main/java/com/project/cafesns/repository/PostRepository.java
@@ -2,11 +2,13 @@ package com.project.cafesns.repository;
 
 import com.project.cafesns.model.entitiy.Cafe;
 import com.project.cafesns.model.entitiy.Post;
+import com.project.cafesns.model.entitiy.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findAllByCafe(Cafe cafe);
+public interface PostListRepository extends JpaRepository<Post, Long> {
 
+    List<Post> findAllByCafeOrderByModifiedAtDesc(Cafe cafe);
+    List<Post> findAllByUserOrderByModifiedAtDesc(User user);
 }
