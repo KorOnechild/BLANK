57b5f9cf1898c7166557f133b87fb5a0d847a5f9
KorOnechild <kyuhong99@gmail.com>
2022-07-21 16:42:41 +0900

    Revert "[chore] PostListRepository와 PostRepository를 병합"
    This reverts commit f0d736204e154ba5949414941882bf1dcc55c002.

--- src/main/java/com/project/cafesns/repository/PostRepository.java
+++ src/main/java/com/project/cafesns/repository/PostRepository.java
@@ -2,14 +2,11 @@ package com.project.cafesns.repository;
 
 import com.project.cafesns.model.entitiy.Cafe;
 import com.project.cafesns.model.entitiy.Post;
-import com.project.cafesns.model.entitiy.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
-    List<Post> findAllByCafeOrderByModifiedAtDesc(Cafe cafe);
-    List<Post> findAllByUserOrderByModifiedAtDesc(User user);
     List<Post> findAllByCafe(Cafe cafe);
+
 }
