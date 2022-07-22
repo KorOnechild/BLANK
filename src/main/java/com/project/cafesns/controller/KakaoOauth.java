1658f34d4e1e95c09cd05584aae09d312d83c827
seungchanKKK <eccsck@gmail.com>
2022-07-23 02:46:57 +0900

    [chore] KakaoOauth Controller 부분 return 값 리다이렉션으로 일단 변경
    리다이렉션 부분 협의후 추가 변경 필요

--- src/main/java/com/project/cafesns/controller/KakaoOauth.java
+++ src/main/java/com/project/cafesns/controller/KakaoOauth.java
@@ -7,13 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 @RequiredArgsConstructor
 public class KakaoOauth {
     private final OauthService oauthService;
 
     @GetMapping("/api/oauth2/kakao")
-    public void getAcToken(@RequestParam ("code") String code){
-        oauthService.getAcToken(code);
+    public void getAcToken(@RequestParam ("code") String code, HttpServletResponse response) throws IOException {
+        String token = oauthService.getAcToken(code);
+        String redirect_uri="https://doridori.shop/?token=" + token;
+        response.sendRedirect(redirect_uri);
     }
+
 }
