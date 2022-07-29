689c314af868d2f73b3050eb69ebf256fc3b2b4c
KorOnechild <kyuhong99@gmail.com>
2022-07-29 17:18:44 +0900

    [chore] 카페 별점 평균 계산 로직 수정
    소수점 첫째자리 까지 나오도록 변경

--- src/main/java/com/project/cafesns/result/CalculatorImp.java
+++ src/main/java/com/project/cafesns/result/CalculatorImp.java
@@ -12,10 +12,10 @@ public class CalculatorImp implements Calculator {
 
     //카페 별점 평균 구하는 함수
     public double getAvgStar(List<Post> postList){
-        float sumStar = 0F;
+        double sumStar = 0.0;
         for(Post post : postList){
             sumStar += post.getStar();
         }
-        return postList.isEmpty() ? 0.0 : Math.floor(sumStar*10)/10.0 / postList.size();
+        return postList.isEmpty() ? 0.0 : Math.floor(sumStar/postList.size() * 10.0)/10.0 ;
     }
 }
